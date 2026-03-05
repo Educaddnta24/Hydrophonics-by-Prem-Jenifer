68e81fafcc0a0cfa53379867c64a9f8b411e8443
Educaddnta24 <aanianandeducaddnta@gmail.com>
2026-03-05 22:27:42 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import React from 'react'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
 import './Style.css'
 const Home = () => {
   return (
